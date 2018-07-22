85c9b20b5d750d53956daeddb9f2867c4e2ea14a
niezhiliang <1132263176@qq.com>
2018-07-22 19:25:32 +0800

    'm'

---
+++ server.js
@@ -0,0 +1,19 @@
+var  ws = require('nodejs-websocket');
+
+var server = ws.createServer(function (conn) {
+
+    conn.on('text',function (str) {
+        console.log(str+"----1")
+    })
+
+    conn.on('close',function (code,reason) {
+        console.log("关闭连接")
+    })
+
+    conn.on('error',function (code,reason) {
+        console.log("异常关闭")
+    })
+    
+}).listen(8080);
+console.log('websocket初始化完成')
+
