6fe2836e1aa2bc08d8416636262e01441cc3731b
niezhiliang <1132263176@qq.com>
2018-07-23 14:55:54 +0800

    m

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var server = ws.createServer(function (conn) {
 
     conn.on('text',function (str) {
         console.log(str+"----1")
+        console.log(server.clients.length+"-6")
+        conn.sendText("520")
     })
 
     conn.on('close',function (code,reason) {
@@ -13,7 +15,7 @@ var server = ws.createServer(function (conn) {
     conn.on('error',function (code,reason) {
         console.log("异常关闭")
     })
-    
+
 }).listen(8080);
 console.log('websocket初始化完成')
 
